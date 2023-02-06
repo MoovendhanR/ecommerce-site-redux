4cec7ca9df3d94143f09f02202731c45e1e58643
MoovendhanR <moovendhanr1011@gmail.com>
2023-02-06 19:28:12 +0530

    login page creaate successfully

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Cart from '../Pages/Cart'
 import HomePage from '../Pages/HomePage'
+import Login from '../Pages/Login'
 import Orders from '../Pages/Orders'
 import Product from '../Pages/Product'
 import Products from '../Pages/Products'
+import Register from '../Pages/Register'
 
 const AllRoutes = () => {
   return (
@@ -15,6 +17,8 @@ const AllRoutes = () => {
         <Route path="/products/:id" element={<Product/>}/>
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/orders" element={<Orders/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
           </Routes>
       
     </div>
